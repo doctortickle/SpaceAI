b9b46cabf5db28f354a0d086ac51164178908e72
Dylan Russell <dyl.russell@gmail.com>
2017-02-05 16:34:27 -0500

    Added GameConstants and Clock classes to common package.

---
+++ src/common/GameConstants.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 Dylan Russell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package common;
+
+/**
+ *
+ * @author Dylan Russell
+ * 
+ * GameConstants not contained in other classes (i.e. ShipType, WeaponType, 
+ * etc.) is stored here.
+ */
+public class GameConstants {
+    // GameConstants class will go here.
+}

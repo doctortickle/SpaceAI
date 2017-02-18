244c88342d6d40b0a56b37085c7456bbec8a8b4d
Dylan Russell <dyl.russell@gmail.com>
2017-02-18 15:30:33 -0500

    Working on updating unit locations.

--- src/common/CastingDirector.java
+++ src/common/CastingDirector.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  *
@@ -34,6 +35,7 @@ public class CastingDirector {
     private final List<Environment> CURRENT_ENVIRONMENT;
     private final List<Actor> COLLIDE_CHECKLIST;
     private final Set<Actor> REMOVED_ACTORS;
+    private final TreeMap<Location,Integer> OBJECT_LOCATIONS;
 
     public CastingDirector() {
         this.REMOVED_ACTORS = new HashSet<>();
@@ -42,6 +44,7 @@ public class CastingDirector {
         this.CURRENT_ENVIRONMENT = new ArrayList<>();
         this.COLLIDE_CHECKLIST = new ArrayList<>();
         this.TO_BE_ADDED = new ArrayList<>();
+        this.OBJECT_LOCATIONS = new TreeMap<>();
     }
     public List<Actor> getToBeAdded() {
         return TO_BE_ADDED;
@@ -128,5 +131,27 @@ public class CastingDirector {
             CURRENT_ENVIRONMENT.remove((Environment)actor);
         });
         REMOVED_ACTORS.clear();
+    }
+    private void buildObjectLocations() {
+        getCurrentUnits().forEach((unit) -> {
+            OBJECT_LOCATIONS.put(unit.getLocation(),unit.getType().getBodyRadius());
+        });
+        getCurrentEnvironment().forEach((environment) -> {
+            OBJECT_LOCATIONS.put(environment.getLocation(), environment.getType().getBodyRadius());
+        });
+    }
+    private void clearObjectLocations() {
+        OBJECT_LOCATIONS.clear();
+    }
+    public TreeMap getObjectLocations() {
+        if(OBJECT_LOCATIONS.isEmpty()) {
+            buildObjectLocations();
+        }
+        return OBJECT_LOCATIONS;
+    }
+    public void resetObjectLocations() {
+        if(OBJECT_LOCATIONS.size() > 0 ) {
+            clearObjectLocations();
+        }
     }
 }
